19ae0337e3b809d0b1ba4643509316ff6c1f6118
Zhansen Zhalel <zhzhzh1806@gmail.com>
2026-07-13 23:58:01 +0500

    end

--- src/raylib_game.c
+++ src/raylib_game.c
@@ -43,6 +43,11 @@ typedef enum {
 
 // TODO: Define your custom data types here
 
+typedef struct Button {
+    Rectangle bounds;
+    bool isPressed;
+} Button;
+
 //----------------------------------------------------------------------------------
 // Global Variables Definition (local to this module)
 //----------------------------------------------------------------------------------
@@ -53,6 +58,29 @@ static RenderTexture2D target = { 0 };  // Render texture to render our game
 static int frameCounter = 0;
 
 // TODO: Define global variables here, recommended to make them static
+#define LIGHTGREEN  (Color){ 155, 188, 15, 255 }
+#define REGULARGREEN (Color){ 139, 172, 15, 255 } 
+#define MELONGREEN (Color){ 48, 98, 48, 255 }
+#define BLACKGREEN (Color){ 15, 56, 15, 255 }
+
+static int mValue = 1;
+static int clickerValue = 1;
+static int multiplierCost = 15;
+static int autoClickerCost = 35;
+static int clickerMultiplierCost = 55;
+
+bool isAutoClickerActive = false;
+static float timer = 0.0f;
+
+static Button melonButton = { 0 };
+static Button melonMultiplierButton = { 0 };
+static Button autoClickerButton = { 0 };
+static Button clickerMultiplierButton = { 0 };
+
+static int melonsCounter = 0;
+static Vector2 mousePoint = { 0.0f, 0.0f };
+
+static Texture2D melonTexture = { 0 };
 
 //----------------------------------------------------------------------------------
 // Module Functions Declaration
@@ -70,7 +98,9 @@ int main(void)
 
     // Initialization
     //--------------------------------------------------------------------------------------
-    InitWindow(screenWidth, screenHeight, "raylib gamejam template");
+    InitWindow(screenWidth, screenHeight, "CATOMELON");
+
+    InitAudioDevice();
     
     // TODO: Load resources / Initialize variables at this point
     
@@ -78,6 +108,13 @@ int main(void)
     // NOTE: If screen is scaled, mouse input should be scaled proportionally
     target = LoadRenderTexture(screenWidth, screenHeight);
     SetTextureFilter(target.texture, TEXTURE_FILTER_BILINEAR);
+
+    melonTexture = LoadTexture("resources/watermelon.png");
+
+    melonButton.bounds = (Rectangle){ 440, 450, 220, 210 };
+    melonMultiplierButton.bounds = (Rectangle){ 60, 170, 270, 70 };
+    autoClickerButton.bounds = (Rectangle){ 60, 280, 270, 70 };
+    clickerMultiplierButton.bounds = (Rectangle){ 60, 280, 270, 70 };
 
 #if defined(PLATFORM_WEB)
     emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
@@ -113,8 +150,52 @@ void UpdateDrawFrame(void)
     // Update
     //----------------------------------------------------------------------------------
     // TODO: Update variables / Implement example logic at this point
-   
+    mousePoint = GetMousePosition();
+    melonButton.isPressed = false;
+    melonMultiplierButton.isPressed = false;
+    autoClickerButton.isPressed = false;
+    clickerMultiplierButton.isPressed = false;
     frameCounter++;
+    timer += GetFrameTime();
+
+    if (CheckCollisionPointRec(mousePoint, melonButton.bounds))
+    {
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) melonButton.isPressed = true;
+    }
+    else if (CheckCollisionPointRec(mousePoint, melonMultiplierButton.bounds))
+    {
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) melonMultiplierButton.isPressed = true;
+    }
+    else if (CheckCollisionPointRec(mousePoint, autoClickerButton.bounds) && isAutoClickerActive == false)
+    {
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) autoClickerButton.isPressed = true;
+    }
+    else if (CheckCollisionPointRec(mousePoint, clickerMultiplierButton.bounds))
+    {
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) clickerMultiplierButton.isPressed = true;
+    }
+
+    if  (melonButton.isPressed) melonsCounter += mValue;
+    if  (melonMultiplierButton.isPressed && melonsCounter >= multiplierCost) {
+        melonsCounter -= multiplierCost;
+        mValue *= 2;
+        multiplierCost *= 5;
+    } 
+    if (autoClickerButton.isPressed && melonsCounter >= autoClickerCost) {
+        melonsCounter -= autoClickerCost;
+        autoClickerCost *= 5;
+        isAutoClickerActive = true;
+    }
+    if (clickerMultiplierButton.isPressed && melonsCounter >= clickerMultiplierCost) {
+        melonsCounter -= clickerMultiplierCost;
+        clickerValue *= 2;
+        clickerMultiplierCost *= 5;
+    }
+
+    if (timer >= 1.0f && isAutoClickerActive) {
+        melonsCounter += clickerValue;
+        timer -= 1.0f;
+    }
     //----------------------------------------------------------------------------------
 
     // Draw
@@ -122,20 +203,26 @@ void UpdateDrawFrame(void)
     // Render game screen to a texture, 
     // it could be useful for scaling or further shader postprocessing
     BeginTextureMode(target);
-        ClearBackground(RAYWHITE);
+        ClearBackground(BLACKGREEN);
         
         // TODO: Draw your game screen here
+        DrawRectangleLinesEx((Rectangle){ 60, 60, 270, 70 }, 10, MELONGREEN);
+        DrawText(TextFormat("%d", melonsCounter), 85, 80, 30, LIGHTGREEN);
 
-        DrawRectangle(70, 90, 200, 200, BLACK);
-        DrawRectangle(70 + 16, 90 + 16, 200 - 32, 200 - 32, RAYWHITE);
-        DrawText("raylib", 70 + 200 - MeasureText("raylib", 40) - 32, 90 + 200 - 40 - 24, 40, BLACK);
+        DrawRectangleLinesEx(melonMultiplierButton.bounds, 10, MELONGREEN);
+        DrawText(TextFormat("MELON x2 - %d", multiplierCost), melonMultiplierButton.bounds.x + 25, melonMultiplierButton.bounds.y + 20, 30, LIGHTGREEN);
 
-        DrawText("6.x", 290, 90 - 26, 280, BLACK);
-        DrawText("GAMEJAM", 70, 90 + 210, 120, MAROON);
+        if (isAutoClickerActive) {
+            DrawRectangleLinesEx(clickerMultiplierButton.bounds, 10, MELONGREEN);
+            DrawText(TextFormat("AUTO x2 - %d", clickerMultiplierCost), clickerMultiplierButton.bounds.x + 25, clickerMultiplierButton.bounds.y + 20, 30, LIGHTGREEN);
+        } else {
+            DrawRectangleLinesEx(autoClickerButton.bounds, 10, MELONGREEN);
+            DrawText(TextFormat("AUTO - %d", autoClickerCost), autoClickerButton.bounds.x + 25, autoClickerButton.bounds.y + 20, 30, LIGHTGREEN);
+        }
+        // DrawRectangleLinesEx(autoClickerButton.bounds, 10, MELONGREEN);
+        // DrawText(TextFormat("AUTO - %d", autoClickerCost), autoClickerButton.bounds.x + 25, autoClickerButton.bounds.y + 20, 30, LIGHTGREEN);
 
-        if ((frameCounter/20)%2) DrawText("are you ready?", 160, 500, 50, BLACK);
-        
-        DrawRectangleLinesEx((Rectangle){ 0, 0, screenWidth, screenHeight }, 16, BLACK);
+        DrawTextureRec(melonTexture, (Rectangle){ 0, 0, melonTexture.width, melonTexture.height }, (Vector2){ melonButton.bounds.x, melonButton.bounds.y }, WHITE);
         
     EndTextureMode();
     
